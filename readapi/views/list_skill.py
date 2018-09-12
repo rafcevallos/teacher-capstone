0055bb46e930d40095df35a11e0d46ae279f2b55
rafcevallos <rafael.cevallos81@gmail.com>
2018-09-12 15:51:00 -0500

    All skills showing and linked to navbar. This was a nightmare but simpler than I thought.

---
+++ readapi/views/list_skill.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext
+from readapi.models import Skill
+
+
+
+@login_required
+def list_skill(request, pk):
+    if request.method == 'GET':
+        skills = Skill.objects.filter(reading_level=pk) # can filter on any object/context
+        return render(request, 'skill/list_skill.html', {'skills': skills})
