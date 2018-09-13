fa59fcba7d9c2bc5a5a7da9952264c2497e621ec
rafcevallos <rafael.cevallos81@gmail.com>
2018-09-13 10:23:25 -0500

    Cleaned up code on navbar

--- readapi/views/list_skill.py
+++ readapi/views/list_skill.py
@@ -5,9 +5,8 @@ from django.template import RequestContext
 from readapi.models import Skill
 
 
-
 @login_required
 def list_skill(request, pk):
     if request.method == 'GET':
-        skills = Skill.objects.filter(reading_level=pk) # can filter on any object/context
+        skills = Skill.objects.filter(reading_level=pk) # can filter on any object/context to find the property I need
         return render(request, 'skill/list_skill.html', {'skills': skills})
